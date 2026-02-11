879049615db32c076e1b8ff2b4bf9015c1cc3a40
zzik2 <cheese.zzik2@gmail.com>
2026-02-12 08:33:24 +0900

    NPE on neoforge fixed

--- neoforge/src/main/java/zzik2/barched/extra/spears/neoforge/BarchedESNeoForge.java
+++ neoforge/src/main/java/zzik2/barched/extra/spears/neoforge/BarchedESNeoForge.java
@@ -1,5 +1,7 @@
 package zzik2.barched.extra.spears.neoforge;
 
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.loading.FMLEnvironment;
 import zzik2.barched.extra.spears.BarchedES;
 import net.neoforged.fml.common.Mod;
@@ -7,12 +9,14 @@ import zzik2.barched.extra.spears.BarchedESClient;
 
 @Mod(BarchedES.MOD_ID)
 public final class BarchedESNeoForge {
-    public BarchedESNeoForge() {
+    public BarchedESNeoForge(IEventBus eventBus) {
         // Run our common setup.
         BarchedES.init();
 
         if (FMLEnvironment.dist.isClient()) {
-            BarchedESClient.init();
+            eventBus.addListener(FMLClientSetupEvent.class, event -> {
+                BarchedESClient.init();
+            });
         }
     }
 }
