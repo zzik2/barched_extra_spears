f630c2d0af02d9d27cda159ac0d81a849177fbfe
zzik2 <cheese.zzik2@gmail.com>
2026-02-12 04:27:44 +0900

    check point 2

--- common/src/main/java/zzik2/barched/extra/spears/BarchedESClient.java
+++ common/src/main/java/zzik2/barched/extra/spears/BarchedESClient.java
@@ -1,6 +1,5 @@
 package zzik2.barched.extra.spears;
 
-import dev.architectury.event.events.common.LifecycleEvent;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
@@ -17,19 +16,14 @@ public class BarchedESClient {
     public static final Map<Item, ModelResourceLocation> SPEAR_IN_HAND_MODEL_MAP = new HashMap<>();
 
     public static void init() {
-        LifecycleEvent.SERVER_STARTED.register(server -> {
-            BarchedES.LOGGER.info("Server Started! Trying register Spear models...");
-            for (String nameSpace : RegisterFactory.REGISTERED_SPEARS.keySet()) {
-                for (RegistrySupplier<Item> item : RegisterFactory.REGISTERED_SPEARS.get(nameSpace)) {
-                    Objects.requireNonNull(item.get());
-                    String materialName = RegisterFactory.REGISTRY_SUPPLIER_TO_MATERIAL.get(item);
-                    SPEAR_MODEL_MAP.put(item.get(), barchedES$spear(nameSpace, materialName));
-                    SPEAR_IN_HAND_MODEL_MAP.put(item.get(), barchedES$spear_in_hand(nameSpace, materialName));
-                }
+        for (String nameSpace : RegisterFactory.REGISTERED_SPEARS.keySet()) {
+            for (RegistrySupplier<Item> item : RegisterFactory.REGISTERED_SPEARS.get(nameSpace)) {
+                Objects.requireNonNull(item.get());
+                String materialName = RegisterFactory.REGISTRY_SUPPLIER_TO_MATERIAL.get(item);
+                SPEAR_MODEL_MAP.put(item.get(), barchedES$spear(nameSpace, materialName));
+                SPEAR_IN_HAND_MODEL_MAP.put(item.get(), barchedES$spear_in_hand(nameSpace, materialName));
             }
-            BarchedES.LOGGER.info("Server Started! Trying register Spear models... Done!");
-        });
-        BarchedES.LOGGER.info("Life Cycle Event Registered!");
+        }
     }
 
     public static ModelResourceLocation barchedES$spear(String nameSpace, String materialName) {
--- common/src/main/java/zzik2/barched/extra/spears/mixin/client/ItemRendererMixin.java
+++ common/src/main/java/zzik2/barched/extra/spears/mixin/client/ItemRendererMixin.java
@@ -1,6 +1,7 @@
 package zzik2.barched.extra.spears.mixin.client;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.ItemModelShaper;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -16,7 +17,6 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import zzik2.barched.extra.spears.BarchedESClient;
 
@@ -24,15 +24,19 @@ import zzik2.barched.extra.spears.BarchedESClient;
 public class ItemRendererMixin {
 
     @Shadow @Final private ItemModelShaper itemModelShaper;
+
+    @Unique private ItemDisplayContext barchedES$itemDisplayContext;
     @Unique private ItemStack barchedES$itemStack;
 
     @Inject(method = "render", at = @At("HEAD"))
     private void barchedES$captureSignature(ItemStack itemStack, ItemDisplayContext itemDisplayContext, boolean bl, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j, BakedModel bakedModel, CallbackInfo ci) {
         this.barchedES$itemStack = itemStack;
+        this.barchedES$itemDisplayContext = itemDisplayContext;
     }
 
-    @ModifyVariable(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/model/BakedModel;getTransforms()Lnet/minecraft/client/renderer/block/model/ItemTransforms;", shift = At.Shift.BEFORE, ordinal = 0), argsOnly = true, index = 8)
-    private BakedModel barchedES$render(BakedModel value, @Local(ordinal = 0) boolean bl2) {
+    @ModifyVariable(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;pushPose()V", shift = At.Shift.AFTER, ordinal = 0), argsOnly = true, index = 8)
+    private BakedModel barchedES$render(BakedModel value) {
+        boolean bl2 = barchedES$itemDisplayContext == ItemDisplayContext.GUI || barchedES$itemDisplayContext == ItemDisplayContext.GROUND || barchedES$itemDisplayContext == ItemDisplayContext.FIXED;
         if (bl2) {
             if (BarchedESClient.SPEAR_MODEL_MAP.containsKey(barchedES$itemStack.getItem())) {
                 ModelResourceLocation model = BarchedESClient.SPEAR_MODEL_MAP.get(barchedES$itemStack.getItem());
@@ -42,12 +46,12 @@ public class ItemRendererMixin {
         return value;
     }
 
-    @Redirect(method = "getModel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemModelShaper;getItemModel(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/client/resources/model/BakedModel;"))
-    private BakedModel barchedES$getModel(ItemModelShaper instance, ItemStack bakedModel) {
+    @WrapOperation(method = "getModel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemModelShaper;getItemModel(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/client/resources/model/BakedModel;"))
+    private BakedModel barchedES$getModel(ItemModelShaper instance, ItemStack bakedModel, Operation<BakedModel> original) {
         if (BarchedESClient.SPEAR_IN_HAND_MODEL_MAP.containsKey(bakedModel.getItem())) {
             ModelResourceLocation model = BarchedESClient.SPEAR_IN_HAND_MODEL_MAP.get(bakedModel.getItem());
             return this.itemModelShaper.getModelManager().getModel(model);
         }
-        return instance.getItemModel(bakedModel);
+        return original.call(instance, bakedModel);
     }
 }
